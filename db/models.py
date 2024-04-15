35173f70feeb9b224bd894e614825b193208ee6b
Vitalii <warcied1@gmail.com>
2024-04-15 15:23:14 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -49,4 +49,5 @@ class MovieSession(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.SET_NULL, null=True)
 
     def __str__(self) -> str:
-        return f"{self.movie} {self.show_time.strftime("%Y-%m-%d %H:%M:%S")}"
+        time_str = self.show_time.strftime("%Y-%m-%d %H:%M:%S")
+        return f"{self.movie} {time_str}"
